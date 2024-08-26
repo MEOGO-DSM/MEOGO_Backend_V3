dbf653ada8864a6e3a056fad0171056f943208d0
soohyeon <meltapplee@gmail.com>
2024-08-26 20:03:51 +0900

    add :: base

---
+++ src/main/kotlin/org/meogo/global/base/BaseUUIDEntity.kt
@@ -0,0 +1,20 @@
+package org.meogo.global.base
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.GenericGenerator
+import java.util.UUID
+
+@MappedSuperclass
+abstract class BaseUUIDEntity(
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(
+        columnDefinition = "BINARY(16)",
+        nullable = false
+    )
+    val id: UUID?
+)
