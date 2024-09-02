74fb48d143221392d79654d09129ddad3465df14
soohyeon <meltapplee@gmail.com>
2024-09-02 16:53:13 +0900

    add :: userNofFound

--- src/main/kotlin/org/meogo/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/org/meogo/global/error/exception/ErrorCode.kt
@@ -7,6 +7,8 @@ enum class ErrorCode(
     INVALID_TOKEN(401, "Invalid Token"),
     EXPIRED_TOKEN(401, "Expired Token"),
 
+    USER_NOT_FOUND(404, "User not found"),
+
     // Internal Server Error
     INTERNAL_SERVER_ERROR(500, "Internal Server Error")
 }
