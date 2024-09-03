fe2b5b705a557d2e693b430dc1938173d4632e31
soohyeon <meltapplee@gmail.com>
2024-09-03 16:43:06 +0900

    modify :: controller

--- src/main/kotlin/org/meogo/domain/review/presentation/ReviewController.kt
+++ src/main/kotlin/org/meogo/domain/review/presentation/ReviewController.kt
@@ -1,21 +1,10 @@
 package org.meogo.domain.review.presentation
 
 import lombok.RequiredArgsConstructor
-import org.meogo.domain.review.presentation.dto.ReviewRequest
-import org.meogo.domain.review.service.CreateReviewService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/review")
-class ReviewController(
-    private val createReviewService: CreateReviewService
-) {
-
-    @PostMapping()
-    fun create(@RequestBody request: ReviewRequest) =
-        createReviewService.execute(request)
-}
+class ReviewController()
