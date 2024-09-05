96e30eed0a0cf267d654de61614fa274af6538dd
soohyeon <meltapplee@gmail.com>
2024-09-05 22:48:21 +0900

    add :: queryReviewPicture api

---
+++ src/main/kotlin/org/meogo/domain/review/service/QueryReviewPictureService.kt
@@ -0,0 +1,22 @@
+package org.meogo.domain.review.service
+
+import org.meogo.domain.review.presentation.dto.response.ReviewPictureResponse
+import org.meogo.domain.review.repository.ReviewRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class QueryReviewPictureService(
+    private val reviewRepository: ReviewRepository
+) {
+
+    @Transactional
+    fun queryReviewPicture(schoolId: Int): List<ReviewPictureResponse> {
+        val reviews = reviewRepository.findAllBySchoolId(schoolId) ?: return emptyList()
+
+        val pictures = reviews
+            .mapNotNull { it.picture?.let { pic -> ReviewPictureResponse(pic) } }
+
+        return pictures
+    }
+}
