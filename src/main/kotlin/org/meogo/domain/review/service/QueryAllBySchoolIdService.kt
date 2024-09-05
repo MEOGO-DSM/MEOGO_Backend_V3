b2cf303bd1ea5af3048020dd8bc6d66310680194
soohyeon <meltapplee@gmail.com>
2024-09-05 18:59:52 +0900

    modify :: userName

--- src/main/kotlin/org/meogo/domain/review/service/QueryAllBySchoolIdService.kt
+++ src/main/kotlin/org/meogo/domain/review/service/QueryAllBySchoolIdService.kt
@@ -2,6 +2,7 @@ package org.meogo.domain.review.service
 
 import org.meogo.domain.review.presentation.dto.response.ReviewResponse
 import org.meogo.domain.review.repository.ReviewRepository
+import org.meogo.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -9,8 +10,10 @@ import java.time.format.DateTimeFormatter
 
 @Service
 class QueryAllBySchoolIdService(
-    private val reviewRepository: ReviewRepository
+    private val reviewRepository: ReviewRepository,
+    private val userFacade: UserFacade
 ) {
+
     @Transactional(readOnly = true)
     fun queryAllBySchoolId(schoolId: Int): List<ReviewResponse> {
         val reviews = reviewRepository.findAllBySchoolId(schoolId)
@@ -18,9 +21,9 @@ class QueryAllBySchoolIdService(
         return reviews.map { review ->
             ReviewResponse(
                 id = review.id,
+                userName = userFacade.getUserById(review.userId).name,
                 content = review.content,
                 date = format(review.date),
-                userName = review.userName,
                 star = review.star,
                 picture = review.picture ?: ""
             )
