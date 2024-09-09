f57e99ed0acb58e767321326b082110aedaee36c
soohyeon <meltapplee@gmail.com>
2024-09-09 20:11:01 +0900

    ktlint

--- src/main/kotlin/org/meogo/domain/post/present/PostController.kt
+++ src/main/kotlin/org/meogo/domain/post/present/PostController.kt
@@ -5,13 +5,13 @@ import org.meogo.domain.post.present.dto.request.PostRequest
 import org.meogo.domain.post.service.CreatePostService
 import org.meogo.domain.post.service.QueryAllPostService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
-import org.springframework.web.bind.annotation.GetMapping
 
 @RequiredArgsConstructor
 @RestController
