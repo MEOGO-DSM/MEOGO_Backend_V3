b51dff91f61d30c88f196104a7ec625635ab6e0e
soohyeon <meltapplee@gmail.com>
2024-09-12 15:01:16 +0900

    refactor :: rename

--- src/main/kotlin/org/meogo/global/s3/S3Config.kt
+++ src/main/kotlin/org/meogo/global/s3/S3Config.kt
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class AwsConfig(
+class S3Config(
     @Value("\${cloud.aws.credentials.access-key}")
     private val accessKey: String,
     @Value("\${cloud.aws.credentials.secret-key}")
