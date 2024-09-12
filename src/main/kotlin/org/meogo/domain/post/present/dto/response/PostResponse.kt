61f5bfe37d0e68b24d35b0dabd687b750fc4ef66
soohyeon <meltapplee@gmail.com>
2024-09-12 14:25:02 +0900

    modify :: queryPost image

--- src/main/kotlin/org/meogo/domain/post/present/dto/response/PostResponse.kt
+++ src/main/kotlin/org/meogo/domain/post/present/dto/response/PostResponse.kt
@@ -1,5 +1,9 @@
 package org.meogo.domain.post.present.dto.response
 
+import org.meogo.domain.post.domain.Post
+import org.meogo.global.s3.FileUtil
+import org.meogo.global.s3.Path
+
 data class PostResponse(
     val id: Long,
     val name: String,
@@ -7,5 +11,20 @@ data class PostResponse(
     val content: String,
     val date: String,
     val keyWord: List<String>?,
-    val schoolId: Int?
-)
+    val schoolId: Int?,
+    val image: String?
+) {
+    constructor(
+        post: Post,
+        fileUtil: FileUtil
+    ) : this(
+        post.id,
+        "익명",
+        post.title,
+        post.content,
+        post.format(post.date),
+        post.keyWord?.split(",")?.map { it.trim() },
+        post.schoolId,
+        post.image?.let { fileUtil.generateObjectUrl(it, Path.COMMUNITY) }
+    )
+}
