7984dd137ecf1f7d8ea9d5cd2e0b9f18695370eb
soohyeon <meltapplee@gmail.com>
2024-09-17 01:12:34 +0900

    delete :: cascade

--- src/main/kotlin/org/meogo/domain/comment/domain/Comment.kt
+++ src/main/kotlin/org/meogo/domain/comment/domain/Comment.kt
@@ -3,7 +3,6 @@ package org.meogo.domain.comment.domain
 import org.meogo.domain.post.domain.Post
 import org.meogo.domain.user.domain.User
 import java.time.LocalDateTime
-import javax.persistence.CascadeType.REMOVE
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
@@ -25,11 +24,11 @@ class Comment(
     @Column(nullable = false)
     val date: LocalDateTime,
 
-    @ManyToOne(cascade = [REMOVE], fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     val post: Post,
 
-    @ManyToOne(cascade = [REMOVE], fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     val user: User
 )
