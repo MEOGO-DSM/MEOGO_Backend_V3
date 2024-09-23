2c935d21d51f390c1f3923049d193d2b2369fd5f
soohyeon <meltapplee@gmail.com>
2024-09-23 09:20:20 +0900

    modify :: querySchoolPost response

--- src/main/kotlin/org/meogo/domain/user/presentation/dto/response/MyPageResponse.kt
+++ src/main/kotlin/org/meogo/domain/user/presentation/dto/response/MyPageResponse.kt
@@ -3,5 +3,6 @@ package org.meogo.domain.user.presentation.dto.response
 data class MyPageResponse(
     val name: String,
     val accountId: String,
+    val enrolledSchool: Int?,
     val profile: String
 )
