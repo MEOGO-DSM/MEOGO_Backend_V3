01abf6db17979171348a96bf28c617159f3e0ba9
soohyeon <meltapplee@gmail.com>
2024-10-21 14:15:58 +0900

    add :: school

---
+++ src/main/kotlin/org/meogo/domain/school/domain/School.kt
@@ -0,0 +1,9 @@
+package org.meogo.domain.school.domain
+
+data class School(
+    val id: Int,
+    val link: String,
+    val schoolGubun: String,
+    val adres: String,
+    val schoolName: String
+)
