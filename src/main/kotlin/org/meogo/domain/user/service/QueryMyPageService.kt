55c36c60e1ddc1b41641ad5185cba2412542ecc3
soohyeon <meltapplee@gmail.com>
2024-10-23 14:20:40 +0900

    modify :: file

--- src/main/kotlin/org/meogo/domain/user/service/QueryMyPageService.kt
+++ src/main/kotlin/org/meogo/domain/user/service/QueryMyPageService.kt
@@ -3,23 +3,26 @@ package org.meogo.domain.user.service
 import org.meogo.domain.user.exception.UserNotFoundException
 import org.meogo.domain.user.facade.UserFacade
 import org.meogo.domain.user.presentation.dto.response.MyPageResponse
+import org.meogo.global.s3.FileUtil
+import org.meogo.global.s3.Path
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryMyPageService(
-    private val userFacade: UserFacade
-
+    private val userFacade: UserFacade,
+    private val fileUtil: FileUtil
 ) {
     @Transactional
     fun execute(): MyPageResponse {
         val user = userFacade.currentUser() ?: throw UserNotFoundException
+        val image = fileUtil.generateObjectUrl(user.profile, Path.USER)
 
         return MyPageResponse(
             user.name,
             user.accountId,
             user.enrolledSchool,
-            user.profile
+            image.substringBefore("?")
         )
     }
 }
