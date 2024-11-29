ff4a047eb81f7277c2127a7fdaffce32da7db46b
soohyeon <meltapplee@gmail.com>
2024-11-29 23:50:10 +0900

    ktlint

--- src/test/kotlin/org/meogo/domain/bookmark/service/BookmarkServiceTest.kt
+++ src/test/kotlin/org/meogo/domain/bookmark/service/BookmarkServiceTest.kt
@@ -1,7 +1,6 @@
 package org.meogo.domain.bookmark.service
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
